58ac8e128e2e664dcb64bb6ee7519be6ea3125f9
walkerte <130520842+walkerte@users.noreply.github.com>
2023-04-11 19:45:14 -0600

    Update fortigate_fqdn_group_output_txt.py

--- fortigate_fqdn_group_output_txt.py
+++ fortigate_fqdn_group_output_txt.py
@@ -1,31 +1,32 @@
+# import the csv module for reading CSV files
 import csv
 
+# specify the input and output file names
 filename = "fqdns.csv"
-group_name = "fqdn_group"
+group_name = "fqdn_group" #change me
 output_file = "fgt_commands.txt"
 
-# Read the FQDNs from the CSV file
+# read the FQDNs from the CSV file using csv.reader()
 with open(filename, 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     fqdns = [row[0] for row in csvreader]
 
-# Generate FortiGate CLI commands for creating FQDN address objects
+# generate FortiGate CLI commands for creating FQDN address objects
 formatted_fqdns = ["edit \"" + fqdn + "\"\n" +
                    "\t" + "set type fqdn\n" +
                    "\t" + "set fqdn \"" + fqdn + "\"\n" +
                    "next\n" for fqdn in fqdns]
 
-# Generate FortiGate CLI commands for creating a group and adding the FQDNs to it
+# generate FortiGate CLI commands for creating a group and adding the FQDNs to it
 group_commands = ["config firewall addrgrp\n" +
                   "edit \"" + group_name + "\"\n"] + \
                  ["\t" + "append member \"" + fqdn + "\"\n" for fqdn in fqdns] + \
-                 ["next\n" +
-                  "end\n"]
+                 ["next\n"]
 
-# Concatenate the CLI commands into a single string
+# concatenate the CLI commands into a single string
 cli_commands = "config firewall address\n" + "".join(formatted_fqdns) + "end\n" + \
-               "".join(group_commands)
+               "".join(group_commands) + "end\n"
 
-# Write the CLI commands to the output file
+# write the CLI commands to the output file
 with open(output_file, 'w') as f:
     f.write(cli_commands)
